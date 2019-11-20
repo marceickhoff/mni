45f1c2c0eea5855a1b6d802b2f1c0d56ea471c4b
marceickhoff <mail@marceickhoff.com>
2019-11-20 12:09:26 +0100

    Simplified main Webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
-let config;
-
 switch(process.env.NODE_ENV) {
 	case 'development':
-		config = require(`./build/webpack.dev.js`);
+		module.exports = require(`./build/webpack.dev.js`);
 		break;
 	case 'production':
-		config = require(`./build/webpack.prod.js`);
+		module.exports = require(`./build/webpack.prod.js`);
 		break;
 }
-module.exports = config;
