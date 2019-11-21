de1f3c1d5b6ed614530d03a9401912bce80c5c7b
marceickhoff <mail@marceickhoff.com>
2019-11-21 08:46:10 +0100

    Modularized common Webpack config

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -2,60 +2,71 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-	entry: [
-		path.resolve("lib/js/core.js"),
-		path.resolve("lib/sass/core.sass")
-	],
-	output: {
-		filename: `js/[name]${(isProd ? ".min" : "")}.js`
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: `css/[name]${(isProd ? ".min" : "")}.css`
-		})
-	],
-	resolve: {
-		alias: {
-			mni: path.resolve("lib/js/modules")
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/,
-				enforce: "pre",
-				use: [
-					{
-						loader: "sass-loader",
-						options: {
-							sassOptions: {
-								outputStyle: "expanded",
-								indentType: "tab",
-								indentWidth: 1,
-								sourceMap: true,
-								linefeed: "crlf",
-								precision: 3
-							},
-						},
-					}
-				]
-			},
-			{
-				test: /\.s[ac]ss$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader
-					},
-					{
-						loader: "css-loader"
-					}
-				]
-			},
-			{
-				test: /\.modernizrrc\.js$/,
-				loader: "webpack-modernizr-loader"
-			}
+const defaultConfig = {
+	styleSyntax: "sass",
+	styleSrc: "lib/sass",
+	styleDist: "dist/css",
+	styleName: "main",
+	scriptSrc: "lib/js",
+	scriptDist: "dist/js",
+	scriptName: "main"
+};
+
+module.exports = function (config) {
+	if (typeof config === "undefined") config = defaultConfig;
+	else config = Object.assign(defaultConfig, config);
+	return {
+		entry: [
+			path.resolve(`${config.scriptSrc}/${config.scriptName}.js`),
+			path.resolve(`${config.styleSrc}/${config.styleName}.${config.styleSyntax}`)
 		],
+		output: {
+			path: path.resolve(__dirname, config.scriptDist),
+			filename: `[name]${(isProd ? ".min" : "")}.js`
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				path: path.resolve(__dirname, config.styleDist),
+				filename: `[name]${(isProd ? ".min" : "")}.css`
+			})
+		],
+		module: {
+			rules: [
+				{
+					test: /\.s[ac]ss$/,
+					enforce: "pre",
+					use: [
+						{
+							loader: "sass-loader",
+							options: {
+								sassOptions: {
+									outputStyle: "expanded",
+									indentType: "tab",
+									indentWidth: 1,
+									sourceMap: true,
+									linefeed: "crlf",
+									precision: 3
+								},
+							},
+						}
+					]
+				},
+				{
+					test: /\.s[ac]ss$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader
+						},
+						{
+							loader: "css-loader"
+						}
+					]
+				},
+				{
+					test: /\.modernizrrc\.js$/,
+					loader: "webpack-modernizr-loader"
+				}
+			],
+		}
 	}
 };
