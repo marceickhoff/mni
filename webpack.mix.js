781d81ad402248b8742357a81fb97fb532136f7b
marceickhoff <mail@marceickhoff.com>
2019-12-02 17:04:02 +0100

    Added boilerplate theme and fixed import order of abstracts

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 eval(require('./bin/util/generator')({
-	srcStyle: 'lib/scss/main.scss',
+	srcStyle: 'lib/scss/themes/_boilerplate.scss',
 	destStyle: 'dist/css/main.css',
 	srcScript: 'lib/js/main.js',
 	destScript: 'dist/js/main.js'
