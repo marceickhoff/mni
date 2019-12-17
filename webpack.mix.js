2c96c669ce0f2466ac6ad5b9974421e59aea61b1
marceickhoff <mail@marceickhoff.com>
2019-12-17 15:11:53 +0100

    Added scaffold and Mix configurations for documentation theme

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,15 @@
+// General settings
 eval(require('./bin/util/generator')({
 	srcStyle: 'lib/scss/themes/_boilerplate.scss',
 	destStyle: 'dist/css/main.css',
 	srcScript: 'lib/js/main.js',
 	destScript: 'dist/js/main.js'
+}));
+
+// Settings for documentation
+eval(require('./bin/util/generator')({
+	srcStyle: 'docs/theme/src/scss/main.scss',
+	destStyle: 'docs/theme/main.css',
+	srcScript: 'docs/theme/src/js/main.js',
+	destScript: 'docs/theme/main.js'
 }));
