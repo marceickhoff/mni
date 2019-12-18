44b9dd95d894e8a98a433c224ce05a597fd57243
marceickhoff <mail@marceickhoff.com>
2019-12-18 16:07:11 +0100

    Removed dist from npm package

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,3 @@
-// General settings
-eval(require('./bin/util/generator')({
-	srcStyle: 'lib/scss/themes/_boilerplate.scss',
-	destStyle: 'dist/css/main.css',
-	srcScript: 'lib/js/main.js',
-	destScript: 'dist/js/main.js'
-}));
-
-// Settings for documentation
 eval(require('./bin/util/generator')({
 	srcStyle: 'docs/theme/src/scss/main.scss',
 	destStyle: 'docs/theme/main.css',
